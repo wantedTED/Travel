4b6ac5d01b970563adda8fb0587421270ca2cc8f
戈鹏飞 <XC17792242376@ICLOUD.COM>
2021-07-15 14:30:50 +0800

    715 1430

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption" v-if="showSwiper">
-      <swiper-slide v-for="item of list" :key="item.id">
-        <img class="swiper-img" :src="item.imgUrl" />
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgurl" />
       </swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -20,7 +20,15 @@ export default {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      }
+      },
+      swiperList: [{
+        id: '0001',
+        imgurl: 'https://imgs.qunarzz.com/piao/fusion/1505/27/301e8692d285a3.jpg'
+
+      }, {
+        id: '0002',
+        imgurl: 'https://imgs.qunarzz.com/sight/p0/1903/7d/7dca9c37a87479eba3.img.jpg_224x148_328c2283.jpg'
+      }]
     }
   },
   computed: {
@@ -32,14 +40,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .wrapper >>> .swiper-pagination-bullet-active
-    background: #fff
-  .wrapper
-    overflow: hidden
-    width: 100%
-    height: 0
-    padding-bottom: 31.25%
-    background: #eee
-    .swiper-img
-      width: 100%
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
 </style>
